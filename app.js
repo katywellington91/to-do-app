20e38a7bf9939b025ad1112ba7c7dfa7f5540970
Katy <katywellington91@gmail.com>
2020-03-24 11:06:36 -0600

    assignment 13 complete

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 
+
 function onReady() {
-  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  let toDos = [];
+  let id = 0;
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
@@ -10,8 +12,11 @@ function onReady() {
 
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
+
+    id++;
 
     newToDoText.value = '';
 
@@ -28,10 +33,22 @@ function onReady() {
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = "Delete!";
+
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        })
+
+        renderTheUI();
+      });
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
     });
 
   }
